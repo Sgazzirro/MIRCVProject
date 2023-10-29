85716d84f8392b3319c2f677913e0a1fee73890c
Sgazzirro <angelodemarco0202@gmail.com>
2023-10-29 12:44:37 +0100

    Version 0.1 Vocabulary.java.

--- src/main/java/it/unipi/model/Vocabulary.java
+++ src/main/java/it/unipi/model/Vocabulary.java
@@ -1,10 +1,59 @@
 package it.unipi.model;
+import java.util.HashMap;
+public class Vocabulary implements VocabularyInteface{
+    private HashMap<String, VocabularyEntry> table;
 
-public interface Vocabulary {
+    public Vocabulary(){
+        table = new HashMap<String, VocabularyEntry>();
+    }
+    public int getDocFrequency(String term) {
+        if(!table.containsKey(term))
+            // Può succedere? Boh
+            return -1;
+        return table.get(term).getFrequency();
+    }
 
-    public int getDocFrequency(String term);
+    public PostingList getPostingList(String term) {
+        if(!table.containsKey(term))
+            // Può succedere? Boh
+            return null;
+        return table.get(term).getPostingList();
+    }
 
-    public PostingList getPostingList(String term);
+    public double getUpperBound(String term) {
+        if(!table.containsKey(term))
+            // Può succedere? Boh
+            return -1;
+        return table.get(term).getUpperBound();
+    }
 
-    public double getUpperBound();
+    private class VocabularyEntry {
+        private Integer frequency;
+        private PostingList postingList;
+        private Double upperBound;
+
+        public Integer getFrequency() {
+            return frequency;
+        }
+
+        public void setFrequency(Integer frequency) {
+            this.frequency = frequency;
+        }
+
+        public PostingList getPostingList() {
+            return postingList;
+        }
+
+        public void setPostingList(PostingList postingList) {
+            this.postingList = postingList;
+        }
+
+        public Double getUpperBound() {
+            return upperBound;
+        }
+
+        public void setUpperBound(Double upperBound) {
+            this.upperBound = upperBound;
+        }
+    }
 }
