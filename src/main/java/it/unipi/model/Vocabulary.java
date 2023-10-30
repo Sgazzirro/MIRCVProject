4259385b2714f20908efc621a54fd4c70cd9edb3
git-marco00 <marco.pardini00@gmail.com>
2023-10-30 16:35:00 +0100

    addEntry

--- src/main/java/it/unipi/model/Vocabulary.java
+++ src/main/java/it/unipi/model/Vocabulary.java
@@ -15,7 +15,17 @@ public class Vocabulary implements VocabularyInterface {
     @Override
     public VocabularyEntry addEntry(String token) {
         // TODO: Create a new entry into the vocabulary
-        return null;
+        if (isPresent(token)) {
+            getEntry(token).setFrequency(getEntry(token).getFrequency() + 1);
+            return getEntry(token);
+        }
+        VocabularyEntry ve = new VocabularyEntry();
+        ve.setFrequency(1);
+        // non so bene cosa intendi per location posting list
+        ve.setLocationPostingList(0);
+        ve.setLocationPostingList(0);
+        table.put(token,ve);
+        return ve;
     }
 
     @Override
