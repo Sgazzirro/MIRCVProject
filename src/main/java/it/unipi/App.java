3f5d92f956ac85b7009b7adc64cdf22c769f2389
git-marco00 <marco.pardini00@gmail.com>
2023-10-30 18:48:24 +0100

    documentStream fatto

--- src/main/java/it/unipi/App.java
+++ src/main/java/it/unipi/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         DocumentStream ds = new DocumentStream("data/reduced_collection.tar.gz");
-        for(int i=0; i<10; i++){
+        for(int i=0; i<5; i++){
             Document doc = ds.nextDoc();
             System.out.println(doc.getId());
             System.out.println(doc.getText());
