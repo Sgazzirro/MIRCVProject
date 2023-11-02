5747c03699c35a8a83e897d0c4019e2f3ccbf11a
Daniele Canzoneri <danielecanzoneri@gmail.com>
2023-11-02 09:54:29 +0100

    Stemming and stopwords removal implementation

--- src/main/java/it/unipi/index/InMemoryIndexing.java
+++ src/main/java/it/unipi/index/InMemoryIndexing.java
@@ -6,9 +6,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import it.unipi.model.implementation.Document;
 import it.unipi.model.*;
 import it.unipi.model.implementation.Vocabulary;
+import opennlp.tools.stemmer.PorterStemmer;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.List;
 
 public class InMemoryIndexing {
@@ -38,10 +43,19 @@ public class InMemoryIndexing {
 
     public void buildIndex(){
         Document document;
+
+        // Stopwords downloaded from https://raw.githubusercontent.com/stopwords-iso/stopwords-en/master/stopwords-en.txt
+        List<String> stopwords = loadStopwords("data/stopwords-en.txt");
+
         while((document = documentStreamInterface.nextDoc())!=null){
             List<String> tokenized = tokenizerInterface.tokenizeBySpace(document.getText());
-            for(String token : tokenized){
-                vocabulary.addEntry(token, document.getId());
+            tokenized.removeAll(stopwords);     // Remove all stopwords
+
+            // Use Porter stemmer
+            PorterStemmer stemmer = new PorterStemmer();
+
+            for (String token : tokenized) {
+                vocabulary.addEntry(stemmer.stem(token), document.getId());
             }
             docIndex.addDocument(document.getId(), tokenized.size());
         }
@@ -60,6 +74,15 @@ public class InMemoryIndexing {
             writerDocIndex.println(docIndexJSON);
         } catch (JsonProcessingException | FileNotFoundException e) {
             e.printStackTrace();
+        }
+    }
+
+    private List<String> loadStopwords(String filename) {
+        try {
+            return Files.readAllLines(new File(filename).toPath(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.err.println("Error loading stopwords");
+            return List.of();
         }
     }
 }
