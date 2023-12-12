af43edc3ebc2c43032e88fe72d7ebc5920d15888
git-marco00 <marco.pardini00@gmail.com>
2023-12-12 18:55:21 +0100

    conjunctive written, not tested

--- src/main/java/it/unipi/scoring/MaxScore.java
+++ src/main/java/it/unipi/scoring/MaxScore.java
@@ -20,89 +20,101 @@ public class MaxScore{
 
     public PriorityQueue<DocumentScore> score(String query, int numResults){
         List<String> queryTokens = tokenizerImpl.tokenizeBySpace(query);
-
         TreeMap<Double, PostingList> treeMap = new TreeMap<>();
 
-        for(String token: queryTokens){
-            VocabularyEntry entry = vocabularyImpl.getEntry(token);
-            if(entry==null){
-                queryTokens.remove(token);
-            } else {
-                treeMap.put(entry.getUpperBound(), entry.getPostingList());
+        // TO DO: PUT IT IN METHOD CALL
+        String mode = "disjunctive";
+
+        ////////////////// DISJUNCTIVE MODE ///////////////////////
+        if(mode.equals("disjunctive")) {
+            for (String token : queryTokens) {
+                VocabularyEntry entry = vocabularyImpl.getEntry(token);
+                if (entry == null) {
+                    queryTokens.remove(token);
+                } else {
+                    treeMap.put(entry.getUpperBound(), entry.getPostingList());
+                }
+            }
+            return maxScore(new ArrayList<>(treeMap.values()), new ArrayList<>(treeMap.keySet()), numResults);
+        }
+        ////////////////// CONJUNCTIVE MODE ///////////////////////
+        else {
+            List<PostingList> postingLists = new ArrayList<>();
+            for (String token : queryTokens) postingLists.add(vocabularyImpl.getEntry(token).getPostingList());
+            return conjunctiveScore(postingLists, numResults);
+        }
+    }
+
+    private PriorityQueue<DocumentScore> conjunctiveScore(List<PostingList> p, int K){
+        PriorityQueue<DocumentScore> scores = new PriorityQueue<>(K);
+        while(nextConjunctive(p)==0){
+            double score = 0;
+            for(PostingList postingList: p){
+                score += postingList.score();
+            }
+            DocumentScore documentScore = new DocumentScore(p.get(0).docId(), score);
+            if(scores.size()<K) scores.add(documentScore);
+            else if (scores.peek().score<score){
+                scores.add(documentScore);
+                scores.poll();
             }
         }
-        return maxScore(new ArrayList<>(treeMap.values()), new ArrayList<>(treeMap.keySet()), numResults);
+        return scores;
     }
 
-    private List<Double> computeUB (List<PostingList> p, List<Double> sigma){
-        List<Double> ub = new ArrayList<>(p.size());
-        ub.add(0, sigma.get(0));
-        for(int i=1; i< p.size(); i++){
-            ub.add(i, (ub.get(i-1)+sigma.get(i)));
-        }
-        return ub;
-    }
-
-    private PriorityQueue<DocumentScore> maxScore(List<PostingList> p, List<Double> sigma, int K){
+    private PriorityQueue<DocumentScore> maxScore(List<PostingList> p, List<Double> sigma, int K) {
         PriorityQueue<DocumentScore> scores = new PriorityQueue<>(K);
         List<Double> ub = computeUB(p, sigma);
         double theta = 0;
         int pivot = 0;
         int current = minimumDocId(p);
-        if(current==-1) return null;
+        if (current == -1) return null;
         int n = p.size();
-        while(pivot < n){
-            double score =0;
+        while (pivot < n) {
+            double score = 0;
             int next = Integer.MAX_VALUE;
-            for(int i=pivot; i<n; i++){
-                if (p.get(i).docId() == current){
+            for (int i = pivot; i < n; i++) {
+                if (p.get(i).docId() == current) {
                     score += p.get(i).score();
                     try {
                         p.get(i).next();
-                    } catch (EOFException e){
+                    } catch (EOFException e) {
                         p.remove(i);
                         sigma.remove(i);
-                        if(p.size()>1) ub = computeUB(p, sigma);
+                        if (p.size() > 1) ub = computeUB(p, sigma);
                         continue;
                     }
                 }
-                if(p.get(i).docId()<next){
+                if (p.get(i).docId() < next) {
                     next = p.get(i).docId();
                 }
             }
-            for(int i=pivot-1; i>0; i--){
-                if(score + ub.get(i)<=theta) break;
+            for (int i = pivot - 1; i > 0; i--) {
+                if (score + ub.get(i) <= theta) break;
                 try {
                     p.get(i).nextGEQ(current);
-                } catch(EOFException e){
+                } catch (EOFException e) {
                     p.remove(i);
                     sigma.remove(i);
-                    if(p.size()>1) ub = computeUB(p, sigma);
+                    if (p.size() > 1) ub = computeUB(p, sigma);
                     continue;
                 }
-                if(p.get(i).docId()==current){
-                    score+=p.get(i).score();
+                if (p.get(i).docId() == current) {
+                    score += p.get(i).score();
                 }
             }
             DocumentScore ds = new DocumentScore(current, score);
-            if (scores.isEmpty() || scores.size()<K || scores.peek().score<score){
+            if (scores.isEmpty() || scores.size() < K || scores.peek().score < score) {
                 scores.add(ds);
-                if(!(scores.size()<K)) scores.poll();
+                if (!(scores.size() < K)) scores.poll();
                 theta = scores.peek().score;
-                while (pivot<n && ub.get(pivot)<=theta){
+                while (pivot < n && ub.get(pivot) <= theta) {
                     pivot++;
                 }
             }
-            current=next;
+            current = next;
         }
         return scores;
-    }
-
-    private boolean areEmpty(List<PostingList> postingLists){
-        for(PostingList postingList:postingLists){
-            if(!postingList.hasNext()) return true;
-        }
-        return false;
     }
 
     private int minimumDocId(List<PostingList> postingLists){
@@ -122,25 +134,44 @@ public class MaxScore{
         return min;
     }
 
-    private int minimumDocIdConjunctive(List<PostingList> postingLists){
-        int minDocId = minimumDocId(postingLists);
-        if (minDocId == -1) return -1;
-        int actualMaxDocId = -1;
-        while(true) {
-            for (PostingList postingList : postingLists) {
-                if(postingList.docId()>actualMaxDocId) actualMaxDocId = postingList.docId();
-            }
-            if (actualMaxDocId == minDocId) return minDocId;        // significa che sono tutte allo stesso docId
-            else {
-                try{
-                    for(PostingList postingList: postingLists){
-                        postingList.nextGEQ(actualMaxDocId);
+    private int nextConjunctive(List<PostingList> postingLists){
+        int current = minimumDocId(postingLists);
+        if (current == -1) return -1;
+        while(true){
+            boolean allToCurrent=true;
+            for(PostingList p: postingLists){
+                if(p.docId()<current){
+                    try{
+                        p.nextGEQ(current);
+                    } catch (EOFException e){
+                        return -1;
                     }
-                    minDocId = actualMaxDocId;
-                } catch (EOFException e){
-                    return -1;
+                }
+                if(p.docId()>current){
+                    current = p.docId();
+                    allToCurrent=false;
+                    break;
                 }
             }
+            if(allToCurrent){
+                return 0;
+            }
         }
+    }
+
+    private List<Double> computeUB (List<PostingList> p, List<Double> sigma){
+        List<Double> ub = new ArrayList<>(p.size());
+        ub.add(0, sigma.get(0));
+        for(int i=1; i< p.size(); i++){
+            ub.add(i, (ub.get(i-1)+sigma.get(i)));
+        }
+        return ub;
+    }
+
+    private boolean areEmpty(List<PostingList> postingLists){
+        for(PostingList postingList:postingLists){
+            if(!postingList.hasNext()) return true;
+        }
+        return false;
     }
 }
