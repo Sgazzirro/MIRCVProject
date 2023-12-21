232a5a837e6309c85067920b24de666e1fc3a528
Daniele Canzoneri <danielecanzoneri@gmail.com>
2023-12-21 11:09:33 +0100

    IO classes cleaning

--- src/main/java/it/unipi/io/DocumentStream.java
+++ src/main/java/it/unipi/io/DocumentStream.java
@@ -3,6 +3,8 @@ package it.unipi.io;
 
 import it.unipi.model.Document;
 import it.unipi.utils.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -10,8 +12,9 @@ import java.util.zip.GZIPInputStream;
 
 public class DocumentStream {
 
+    protected static final Logger logger = LoggerFactory.getLogger(DocumentStream.class);
+
     private BufferedReader br;
-    private boolean firstLine = true;
 
     private final String filename;
 
@@ -19,81 +22,71 @@ public class DocumentStream {
     private final int endDocId;     // docId of the first document not to be processed (-1 by default -> EOF)
     private int offset;
 
-    public DocumentStream(String filename, int startDocId, int endDocId) {
+    public DocumentStream(String filename, int startDocId, int endDocId) throws IOException {
         this.filename = filename;
         this.startDocId = startDocId;
         this.endDocId = (endDocId >= 0) ? endDocId : Integer.MAX_VALUE;
         reset();
     }
 
-    public DocumentStream(String filename) {
+    public DocumentStream(String filename) throws IOException {
         this(filename, 0, -1);
     }
 
-    public DocumentStream() {
+    public DocumentStream() throws IOException {
         this(Constants.COLLECTION_FILE, 0, -1);
     }
 
-    private void reset() {
-        try {
-            FileInputStream fileInput = new FileInputStream(filename);
+    private void reset() throws IOException {
+        FileInputStream fileInput = new FileInputStream(filename);
 
-            // Check if file is compressed
-            InputStream inputStream = fileInput;
-            if (filename.endsWith(".tar.gz"))
-                inputStream = new GZIPInputStream(fileInput);
-
-            // Forces Unicode decoding (it should be on by default)
-            InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-            br = new BufferedReader(reader);
-
-            // Set document stream start to startDocId
-            offset = 0;
-            while (offset < startDocId) {
-                br.readLine();
-                offset++;
-            }
-
-        } catch (IOException e){
-            e.printStackTrace();
+        // Check if file is compressed
+        InputStream inputStream = fileInput;
+        if (filename.endsWith(".tar.gz")) {
+            logger.info("Compressed file " + filename + " correctly decompressed");
+            inputStream = new GZIPInputStream(fileInput);
         }
+
+        // Forces Unicode decoding (it should be on by default)
+        InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        br = new BufferedReader(reader);
+
+        // Set document stream start to startDocId
+        offset = 0;
+        while (offset < startDocId) {
+            br.readLine();
+            offset++;
+        }
+
+        logger.info("Stream correctly initialized");
     }
 
-    public Document nextDoc() {
-        Document doc = new Document();
-        try {
-            // Check if we are at the end of the stream
-            if (offset == endDocId)
-                return null;
-
-            String line = br.readLine();
-            offset++;
-            if (line == null)
-                return null;
-            String[] data = line.split("\t"); // Split the line into fields using the tab character
-
-            doc.setId(Integer.parseInt(data[0]));
-            doc.setText(data[1]);
-
-        } catch (IOException e) {
-            e.printStackTrace();
+    public Document nextDoc() throws IOException {
+        // Check if we are at the end of the stream
+        if (offset == endDocId)
             return null;
-        }
+
+        Document doc = new Document();
+
+        String line = br.readLine();
+        offset++;
+        if (line == null)
+            return null;
+        String[] data = line.split("\t"); // Split the line into fields using the tab character
+
+        doc.setId(Integer.parseInt(data[0]));
+        doc.setText(data[1]);
+
         return doc;
     }
 
-    public Document getDoc(int docId) {
+    public Document getDoc(int docId) throws IOException {
         reset();
 
-        try {
-            while (docId > 0) {
-                br.readLine();
-                firstLine = false;
-                docId--;
-            }
-            return nextDoc();
-        } catch (IOException ignored) { }
-
-        return null;
+        while (docId > 0) {
+            br.readLine();
+            docId--;
+        }
+        return nextDoc();
     }
 }
